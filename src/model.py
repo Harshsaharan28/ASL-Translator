b20e59f9c9b3641b19d70723f52ffeb4303c8246
Harsh Saharan <157204525+Harshsaharan28@users.noreply.github.com>
2025-06-23 03:35:10 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -7,7 +7,7 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-df = pd.read_csv('landmark_data.csv')  
+df = pd.read_csv('../data/landmark_data.csv')  
 
 X = df.iloc[:, 1:].values  
 y = df['label'].values    
@@ -15,12 +15,12 @@ y = df['label'].values
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
-joblib.dump(scaler, 'scaler.pkl')
+joblib.dump(scaler, '../models/scaler.pkl')
 
 label_encoder = LabelEncoder()
 y_encoded = label_encoder.fit_transform(y)
 
-joblib.dump(label_encoder, 'label_encoder.pkl')
+joblib.dump(label_encoder, '../models/label_encoder.pkl')
 
 print("Labels:", np.unique(y))
 print("Encoded Labels:", np.unique(y_encoded))
@@ -28,7 +28,6 @@ print("Samples per class:\n", pd.Series(y).value_counts())
 
 X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=42)
 
-# Build the model
 model = Sequential([
     Dense(128, input_dim=X_train.shape[1], activation='relu'),
     Dense(64, activation='relu'),
@@ -44,4 +43,4 @@ history = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_data
 loss, accuracy = model.evaluate(X_test, y_test)
 print(f"Test accuracy: {accuracy:.4f}")
 
-model.save('sign_language_model.keras')
+model.save('../models/sign_language_model.keras')
